01b324a99fdf9891235a4123afd9275c438fea65
Andrew <106198626+lxvdev@users.noreply.github.com>
2024-11-21 23:18:32 +0100

    Improved InputDialog

--- Edulink.Client/Dialogs/InputDialog.xaml.cs
+++ Edulink.Client/Dialogs/InputDialog.xaml.cs
@@ -9,11 +9,12 @@ namespace Edulink
     public partial class InputDialog : Window
     {
         public string InputValue { get; private set; }
+        public InputDialogResult Result { get; private set; }
 
-        public InputDialog(string dialogText, string caption)
+        public InputDialog(string dialogHeader, string caption)
         {
             InitializeComponent();
-            DialogText.Text = dialogText;
+            DialogText.Text = dialogHeader;
             Title = caption;
             InputTextBox.Focus();
         }
@@ -21,6 +22,7 @@ namespace Edulink
         private void SendInput()
         {
             InputValue = InputTextBox.Text;
+            Result = InputDialogResult.Ok;
             DialogResult = true;
             Close();
         }
@@ -32,16 +34,37 @@ namespace Edulink
 
         private void CancelButton_Click(object sender, RoutedEventArgs e)
         {
-            DialogResult = false;
+            Result = InputDialogResult.Cancel;
+            DialogResult = true;
             Close();
         }
 
-        private void InputTextBox_PreviewKeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        private void InputTextBox_PreviewKeyUp(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Enter)
             {
                 SendInput();
             }
         }
+
+        public static (InputDialogResult result, string input) Show(string dialogHeader, string caption)
+        {
+            InputDialog dialog = new InputDialog(dialogHeader, caption);
+            bool? result = dialog.ShowDialog();
+
+            if (result == true)
+            {
+                return (dialog.Result, dialog.InputValue);
+            }
+
+            return (InputDialogResult.None, null);
+        }
+    }
+
+    public enum InputDialogResult
+    {
+        None,
+        Ok,
+        Cancel
     }
 }
