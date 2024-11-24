f8e6b112f6a31a26109acf2f246756554b52ce9a
Andrew <106198626+lxvdev@users.noreply.github.com>
2024-11-24 23:08:31 +0100

    Added TaskExtensions.cs for timeout

---
+++ Edulink.TCPHelper/Extensions/TaskExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Edulink.Classes
+{
+    public static class TaskExtensions
+    {
+        public static async Task<T> WithCancellation<T>(this Task<T> task, CancellationToken cancellationToken)
+        {
+            var completedTask = await Task.WhenAny(task, Task.Delay(Timeout.Infinite, cancellationToken));
+            if (completedTask == task)
+            {
+                return await task;
+            }
+            else
+            {
+                throw new OperationCanceledException(cancellationToken);
+            }
+        }
+    }
+}
