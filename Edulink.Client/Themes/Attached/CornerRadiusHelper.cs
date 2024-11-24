3eb0b6a2b7913b0d6f66d4cee9bdf156b5582e32
Andrew <106198626+lxvdev@users.noreply.github.com>
2024-11-24 18:24:34 +0100

    Property for corner radius for any type of control

---
+++ Edulink.Client/Themes/Attached/CornerRadiusHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace Edulink.Attached
+{
+    public static class CornerRadiusHelper
+    {
+        public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.RegisterAttached(
+            "CornerRadius",
+            typeof(CornerRadius),
+            typeof(CornerRadiusHelper),
+            new PropertyMetadata(new CornerRadius(0)));
+
+        public static CornerRadius GetCornerRadius(DependencyObject obj) => (CornerRadius)obj.GetValue(CornerRadiusProperty);
+
+        public static void SetCornerRadius(DependencyObject obj, CornerRadius value) => obj.SetValue(CornerRadiusProperty, value);
+    }
+}
