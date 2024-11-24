b5f450dc035f92caa58d476e10e4f7cb0712eaf5
Andrew <106198626+lxvdev@users.noreply.github.com>
2024-11-24 18:25:24 +0100

    Placeholder property for textbox and passwordbox

---
+++ Edulink.Client/Themes/Attached/PlaceholderHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace Edulink.Attached
+{
+    public static class PlaceholderHelper
+    {
+        public static readonly DependencyProperty PlaceholderProperty = DependencyProperty.RegisterAttached(
+            "Placeholder",
+            typeof(string),
+            typeof(PlaceholderHelper),
+            new PropertyMetadata(string.Empty));
+
+        public static string GetPlaceholder(DependencyObject d) => (string)d.GetValue(PlaceholderProperty);
+
+        public static void SetPlaceholder(DependencyObject d, string value) => d.SetValue(PlaceholderProperty, value);
+    }
+}
