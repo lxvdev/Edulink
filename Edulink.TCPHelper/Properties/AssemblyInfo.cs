04399d8b6102e14bfc0aa5a01facf04e08cc9f31
Andrew <106198626+lxvdev@users.noreply.github.com>
2024-11-24 23:13:03 +0100

    Updated AssemblyInfo and csproj

--- Edulink.TCPHelper/Properties/AssemblyInfo.cs
+++ Edulink.TCPHelper/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
@@ -10,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Edulink.TCPHelper")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyCopyright("Copyright © Andrew 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
