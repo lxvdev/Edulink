abc52d296a6291939418c0b6ead9392cb6a32c01
Andrew <106198626+lxvdev@users.noreply.github.com>
2024-11-25 22:02:59 +0100

    Created TrayIconViewModel.cs

---
+++ Edulink.Client/ViewModels/TrayIconViewModel.cs
@@ -0,0 +1,64 @@
+﻿using Edulink.Client;
+using Edulink.Commands;
+using System.ComponentModel;
+using System.Reflection;
+using System.Windows.Input;
+
+namespace Edulink.ViewModels
+{
+    public class TrayIconViewModel : INotifyPropertyChanged
+    {
+        private string _connectionStatus;
+
+        public string ConnectionStatus
+        {
+            get => _connectionStatus;
+            set
+            {
+                if (_connectionStatus != value)
+                {
+                    _connectionStatus = value;
+                    OnPropertyChanged(nameof(ConnectionStatus));
+                }
+            }
+        }
+
+        public string Version => $"v{Assembly.GetExecutingAssembly().GetName().Version}";
+        //public string ConnectionStatus = (string)App.Current.TryFindResource((bool)(App.Client?.Connected) ? "TrayContextMenu.Status.Connected" : "TrayContextMenu.Status.Disconnected");
+
+        public ICommand SettingsCommand => new RelayCommand(OpenSettings);
+        public ICommand AboutCommand => new RelayCommand(OpenAbout);
+        public ICommand ExitCommand => new RelayCommand(ExitApplication);
+
+        private void OpenSettings()
+        {
+            if (App.VerifyAdminRights())
+            {
+                SettingsWindow settingsWindow = new SettingsWindow();
+                settingsWindow.Show();
+            }
+        }
+
+        private void OpenAbout()
+        {
+            AboutDialog aboutDialog = new AboutDialog();
+            aboutDialog.Show();
+        }
+
+        private void ExitApplication()
+        {
+            if (App.VerifyAdminRights())
+            {
+                App.CloseApp();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
