238415010a588e0b861c7641a38fc2fd16290ec0
Andrew <106198626+lxvdev@users.noreply.github.com>
2025-02-14 23:57:27 +0100

    Updated About dialog with view model
    - Implemented the code-behind in `AboutDialog.xaml.cs` to
    initialize the view model and handle close requests.
    - Defined `AboutDialogViewModel.cs` to manage data binding,
    initialize properties from assembly attributes, and provide
    commands for closing the dialog and opening the Github page.

---
+++ Edulink.Server/Dialogs/AboutDialog.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Edulink.ViewModels;
+using System;
+using System.Windows;
+namespace Edulink
+{
+    /// <summary>
+    /// Interaction logic for AboutWindow.xaml
+    /// </summary>
+    public partial class AboutDialog : Window
+    {
+        public AboutDialog()
+        {
+            InitializeComponent();
+            AboutDialogViewModel aboutDialogViewModel = new AboutDialogViewModel();
+            aboutDialogViewModel.RequestClose += OnRequestClose;
+            DataContext = aboutDialogViewModel;
+        }
+
+        private void OnRequestClose(object sender, EventArgs e)
+        {
+            if (sender is AboutDialogViewModel viewModel)
+            {
+                Close();
+            }
+        }
+    }
+}
---
+++ Edulink.Server/ViewModels/AboutDialogViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Edulink.MVVM;
+using System;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace Edulink.ViewModels
+{
+    public class AboutDialogViewModel
+    {
+        public string Product { get; set; }
+        public string Description { get; set; }
+        public string Version { get; set; }
+        public string Copyright { get; set; }
+
+        public AboutDialogViewModel()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            Product = $"{assembly.GetCustomAttribute<AssemblyProductAttribute>().Product}";
+            Description = $"{assembly.GetCustomAttribute<AssemblyDescriptionAttribute>().Description}";
+            Version = $"v{assembly.GetName().Version}";
+            Copyright = $"{assembly.GetCustomAttribute<AssemblyCopyrightAttribute>().Copyright}";
+        }
+
+        public RelayCommand CloseCommand => new RelayCommand(execute => OnRequestClose());
+
+        public RelayCommand GithubCommand => new RelayCommand(execute => Github());
+        private void Github()
+        {
+            Process.Start("https://github.com/lxvdev/Edulink");
+        }
+
+        public event EventHandler RequestClose;
+        protected virtual void OnRequestClose()
+        {
+            RequestClose?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
