40407d2310c7844d3cc758eb70dd341cd9758965
Andrew <106198626+lxvdev@users.noreply.github.com>
2025-02-14 21:17:29 +0100

    Add BooleanToSortDirectionConverter and InverseBooleanConverter
    Implemented two new converter classes in the Edulink.Converters namespace.
    The BooleanToSortDirectionConverter handles conversion between ListSortDirection and boolean values, while the InverseBooleanConverter inverts boolean values during conversion.

---
+++ Edulink.Server/Converters/BooleanToSortDirectionConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Edulink.Converters
+{
+    public class BooleanToSortDirectionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is ListSortDirection direction)
+            {
+                return direction == ListSortDirection.Descending;
+            }
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool isChecked)
+            {
+                return isChecked ? ListSortDirection.Descending : ListSortDirection.Ascending;
+            }
+            return ListSortDirection.Ascending;
+        }
+    }
+}
