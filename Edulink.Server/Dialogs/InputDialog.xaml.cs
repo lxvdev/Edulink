22fcb10c800e260804a92e8d35f78c4390431639
Andrew <106198626+lxvdev@users.noreply.github.com>
2025-02-14 23:34:17 +0100

    Refactor InputDialog for MVVM pattern and Material Design
    - Updated InputDialog.xaml to improve layout with Material Design and implement data binding for title and message.
    - Refactored button handling to use commands from the new InputDialogViewModel.
    - Added InputDialogViewModel.cs to encapsulate dialog logic, including properties for input value and commands for OK and Cancel actions.

--- Edulink.Server/Dialogs/InputDialog.xaml.cs
+++ Edulink.Server/Dialogs/InputDialog.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using Edulink.ViewModels;
+using System.Windows;
 using System.Windows.Input;
 
 namespace Edulink
@@ -8,40 +9,72 @@ namespace Edulink
     /// </summary>
     public partial class InputDialog : Window
     {
+        public InputDialogButtonResult ButtonResult { get; private set; }
         public string InputValue { get; private set; }
 
-        public InputDialog(string dialogText, string caption)
+        public InputDialog(string message, string title)
         {
             InitializeComponent();
-            DialogText.Text = dialogText;
-            Title = caption;
+            InputDialogViewModel inputDialogViewModel = new InputDialogViewModel(message, title);
+            inputDialogViewModel.RequestClose += OnRequestClose;
+            DataContext = inputDialogViewModel;
+
             InputTextBox.Focus();
         }
 
-        private void SendInput()
+        private void InputTextBox_PreviewKeyUp(object sender, KeyEventArgs e)
         {
-            InputValue = InputTextBox.Text;
-            DialogResult = true;
-            Close();
-        }
+            if (!(e.Key == Key.Enter))
+                return;
 
-        private void OkButton_Click(object sender, RoutedEventArgs e)
-        {
-            SendInput();
-        }
-
-        private void CancelButton_Click(object sender, RoutedEventArgs e)
-        {
-            DialogResult = false;
-            Close();
-        }
-
-        private void InputTextBox_PreviewKeyUp(object sender, System.Windows.Input.KeyEventArgs e)
-        {
-            if (e.Key == Key.Enter)
+            if (DataContext is InputDialogViewModel viewModel)
             {
-                SendInput();
+                if (!viewModel.OkCommand.CanExecute(null))
+                    return;
+
+                viewModel.OkCommand.Execute(null);
             }
         }
+
+        private void OnRequestClose(object sender, bool dialogResult)
+        {
+            if (sender is InputDialogViewModel viewModel)
+            {
+                ButtonResult = viewModel.ButtonResult;
+                InputValue = viewModel.InputValue;
+                DialogResult = dialogResult;
+                Close();
+            }
+        }
+
+        public static InputDialogResult Show(string message, string title)
+        {
+            InputDialog dialog = new InputDialog(message, title);
+            if (dialog.ShowDialog() == true)
+            {
+                return new InputDialogResult(dialog.ButtonResult, dialog.InputValue);
+            }
+
+            return new InputDialogResult(dialog.ButtonResult);
+        }
+    }
+
+    public class InputDialogResult
+    {
+        public InputDialogButtonResult ButtonResult { get; set; }
+        public string ReplyResult { get; set; }
+
+        public InputDialogResult(InputDialogButtonResult buttonResult, string reply = null)
+        {
+            ButtonResult = buttonResult;
+            ReplyResult = reply;
+        }
+    }
+
+    public enum InputDialogButtonResult
+    {
+        None,
+        Ok,
+        Cancel
     }
 }
---
+++ Edulink.Server/ViewModels/InputDialogViewModel.cs
@@ -0,0 +1,55 @@
+﻿using Edulink.MVVM;
+using System;
+
+namespace Edulink.ViewModels
+{
+    internal class InputDialogViewModel : ViewModelBase
+    {
+        public string Message { get; private set; }
+        public string Title { get; private set; }
+
+        public InputDialogButtonResult ButtonResult { get; private set; }
+
+        private string _inputValue;
+        public string InputValue
+        {
+            get => _inputValue;
+            set
+            {
+                if (_inputValue != value)
+                {
+                    _inputValue = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public InputDialogViewModel(string message, string title)
+        {
+            Message = message;
+            Title = title;
+        }
+
+        #region Commands    
+        public RelayCommand OkCommand => new RelayCommand(execute => Ok(), canExecute => !string.IsNullOrEmpty(InputValue));
+        private void Ok()
+        {
+            ButtonResult = InputDialogButtonResult.Ok;
+            OnRequestClose(true);
+        }
+
+        public RelayCommand CancelCommand => new RelayCommand(execute => Cancel());
+        private void Cancel()
+        {
+            ButtonResult = InputDialogButtonResult.Cancel;
+            OnRequestClose(true);
+        }
+        #endregion
+
+        public event EventHandler<bool> RequestClose;
+        protected virtual void OnRequestClose(bool dialogResult)
+        {
+            RequestClose?.Invoke(this, dialogResult);
+        }
+    }
+}
