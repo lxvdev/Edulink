dcaf0bb2680adf5f7968fc80d50629217f60fb84
Andrew <106198626+lxvdev@users.noreply.github.com>
2025-02-22 16:09:23 +0100

    Refactor MessageDialogViewModel for improved structure
    Updated namespace and removed MessageDialog model.
    Replaced model properties with direct properties in
    MessageDialogViewModel. Modified methods to manipulate
    new properties directly. Changed command properties to
    return ICommand for better compatibility. Simplified
    button result handling and updated user input processing
    in the Reply method.

--- Edulink.Server/ViewModels/MessageDialogViewModel.cs
+++ Edulink.Server/ViewModels/MessageDialogViewModel.cs
@@ -1,20 +1,35 @@
-﻿using Edulink.Server.MVVM;
+﻿using Edulink.MVVM;
+using Edulink.Views;
 using MaterialDesignThemes.Wpf;
 using System;
 using System.Windows;
+using System.Windows.Input;
 using System.Windows.Media;
 
-namespace Edulink.Server.ViewModels
+namespace Edulink.ViewModels
 {
     public class MessageDialogViewModel
     {
-        private Models.MessageDialog _messageDialog = new Models.MessageDialog();
-        public Models.MessageDialog MessageDialog => _messageDialog;
+        public string Message { get; set; }
+        public string Title { get; set; }
+
+        public PackIconKind IconKind { get; set; } = PackIconKind.None;
+        public SolidColorBrush IconColor { get; set; } = new SolidColorBrush(Colors.Black);
+        public Visibility IconVisibility { get; set; } = Visibility.Collapsed;
+
+        public Visibility OkButtonVisibility { get; set; } = Visibility.Collapsed;
+        public Visibility ReplyButtonVisibility { get; set; } = Visibility.Collapsed;
+        public Visibility CancelButtonVisibility { get; set; } = Visibility.Collapsed;
+        public Visibility YesButtonVisibility { get; set; } = Visibility.Collapsed;
+        public Visibility NoButtonVisibility { get; set; } = Visibility.Collapsed;
+
+        public MessageDialogButtonResult ButtonResult { get; set; }
+        public string ReplyResult { get; set; }
 
         public MessageDialogViewModel(string message, string title, MessageDialogButton button, MessageDialogIcon icon)
         {
-            _messageDialog.Message = message;
-            _messageDialog.Title = title;
+            Message = message;
+            Title = title;
             SetMessageIcon(icon);
             SetButtonVisibility(button);
         }
@@ -25,27 +40,27 @@ namespace Edulink.Server.ViewModels
             switch (icon)
             {
                 case MessageDialogIcon.Error:
-                    _messageDialog.IconKind = PackIconKind.Error;
-                    _messageDialog.IconColor = new SolidColorBrush(Colors.Red);
-                    _messageDialog.IconVisibility = Visibility.Visible;
+                    IconKind = PackIconKind.Error;
+                    IconColor = new SolidColorBrush(Colors.Red);
+                    IconVisibility = Visibility.Visible;
                     break;
                 case MessageDialogIcon.Information:
-                    _messageDialog.IconKind = PackIconKind.Info;
-                    _messageDialog.IconColor = new SolidColorBrush(Colors.RoyalBlue);
-                    _messageDialog.IconVisibility = Visibility.Visible;
+                    IconKind = PackIconKind.Info;
+                    IconColor = new SolidColorBrush(Colors.RoyalBlue);
+                    IconVisibility = Visibility.Visible;
                     break;
                 case MessageDialogIcon.Warning:
-                    _messageDialog.IconKind = PackIconKind.Warning;
-                    _messageDialog.IconColor = new SolidColorBrush(Colors.Orange);
-                    _messageDialog.IconVisibility = Visibility.Visible;
+                    IconKind = PackIconKind.Warning;
+                    IconColor = new SolidColorBrush(Colors.Orange);
+                    IconVisibility = Visibility.Visible;
                     break;
                 case MessageDialogIcon.Success:
-                    _messageDialog.IconKind = PackIconKind.CheckCircle;
-                    _messageDialog.IconColor = new SolidColorBrush(Colors.Green);
-                    _messageDialog.IconVisibility = Visibility.Visible;
+                    IconKind = PackIconKind.CheckCircle;
+                    IconColor = new SolidColorBrush(Colors.Green);
+                    IconVisibility = Visibility.Visible;
                     break;
                 default:
-                    _messageDialog.IconVisibility = Visibility.Collapsed;
+                    IconVisibility = Visibility.Collapsed;
                     break;
             }
         }
@@ -55,25 +70,25 @@ namespace Edulink.Server.ViewModels
             switch (button)
             {
                 case MessageDialogButton.Ok:
-                    _messageDialog.OkButtonVisibility = Visibility.Visible;
+                    OkButtonVisibility = Visibility.Visible;
                     break;
                 case MessageDialogButton.Reply:
-                    _messageDialog.ReplyButtonVisibility = Visibility.Visible;
+                    ReplyButtonVisibility = Visibility.Visible;
                     break;
                 case MessageDialogButton.Cancel:
-                    _messageDialog.CancelButtonVisibility = Visibility.Visible;
+                    CancelButtonVisibility = Visibility.Visible;
                     break;
                 case MessageDialogButton.YesNo:
-                    _messageDialog.YesButtonVisibility = Visibility.Visible;
-                    _messageDialog.NoButtonVisibility = Visibility.Visible;
+                    YesButtonVisibility = Visibility.Visible;
+                    NoButtonVisibility = Visibility.Visible;
                     break;
                 case MessageDialogButton.OkCancel:
-                    _messageDialog.OkButtonVisibility = Visibility.Visible;
-                    _messageDialog.CancelButtonVisibility = Visibility.Visible;
+                    OkButtonVisibility = Visibility.Visible;
+                    CancelButtonVisibility = Visibility.Visible;
                     break;
                 case MessageDialogButton.OkReply:
-                    _messageDialog.OkButtonVisibility = Visibility.Visible;
-                    _messageDialog.ReplyButtonVisibility = Visibility.Visible;
+                    OkButtonVisibility = Visibility.Visible;
+                    ReplyButtonVisibility = Visibility.Visible;
                     break;
                 default:
                     break;
@@ -82,44 +97,44 @@ namespace Edulink.Server.ViewModels
         #endregion
 
         #region Commands
-        public RelayCommand OkCommand => new RelayCommand(execute => Ok());
+        public ICommand OkCommand => new RelayCommand(execute => Ok());
         private void Ok()
         {
-            _messageDialog.ButtonResult = MessageDialogButtonResult.Ok;
+            ButtonResult = MessageDialogButtonResult.Ok;
             OnRequestClose(true);
         }
 
-        public RelayCommand ReplyCommand => new RelayCommand(execute => Reply());
+        public ICommand ReplyCommand => new RelayCommand(execute => Reply());
         private void Reply()
         {
             InputDialogResult result = InputDialog.Show((string)Application.Current.TryFindResource("Input.Content.SendMessage"),
                                                         (string)Application.Current.TryFindResource("Input.Title.SendMessage"));
-            if (result.ButtonResult == InputDialogButtonResult.Ok && !string.IsNullOrEmpty(result.ReplyResult))
+            if (result.ButtonResult == InputDialogButtonResult.Ok && !string.IsNullOrEmpty(result.InputResult))
             {
-                _messageDialog.ReplyResult = result.ReplyResult;
-                _messageDialog.ButtonResult = MessageDialogButtonResult.Reply;
+                ReplyResult = result.InputResult;
+                ButtonResult = MessageDialogButtonResult.Reply;
                 OnRequestClose(true);
             }
         }
 
-        public RelayCommand CancelCommand => new RelayCommand(execute => Cancel());
+        public ICommand CancelCommand => new RelayCommand(execute => Cancel());
         private void Cancel()
         {
-            _messageDialog.ButtonResult = MessageDialogButtonResult.Cancel;
+            ButtonResult = MessageDialogButtonResult.Cancel;
             OnRequestClose(true);
         }
 
-        public RelayCommand YesCommand => new RelayCommand(execute => Yes());
+        public ICommand YesCommand => new RelayCommand(execute => Yes());
         private void Yes()
         {
-            _messageDialog.ButtonResult = MessageDialogButtonResult.Yes;
+            ButtonResult = MessageDialogButtonResult.Yes;
             OnRequestClose(true);
         }
 
-        public RelayCommand NoCommand => new RelayCommand(execute => No());
+        public ICommand NoCommand => new RelayCommand(execute => No());
         private void No()
         {
-            _messageDialog.ButtonResult = MessageDialogButtonResult.No;
+            ButtonResult = MessageDialogButtonResult.No;
             OnRequestClose(true);
         }
         #endregion
