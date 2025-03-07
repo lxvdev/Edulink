3e6069ede93bd06238b3e851fb7ad15d50240f28
Andrew <106198626+lxvdev@users.noreply.github.com>
2025-03-07 23:13:20 +0100

    Add server project note and Settings class
    - Added `NOTE.txt` to guide users on accessing server project files.
    - Introduced `Settings` class in `Settings.cs` with properties for configuration, including default values.

---
+++ Edulink.Client/Models/Settings.cs
@@ -0,0 +1,13 @@
+﻿namespace Edulink.Models
+{
+    public class Settings
+    {
+        public string Name { get; set; } = null;
+        public string IPAddress { get; set; } = null;
+        public int Port { get; set; } = 7153;
+        public bool CheckForUpdates { get; set; } = true;
+        public string Language { get; set; } = null;
+        public string Password { get; set; } = null;
+        public string Theme { get; set; } = "Auto";
+    }
+}
