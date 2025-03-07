92a842e9aeea177416aad059646c79a524941e4f
Andrew <106198626+lxvdev@users.noreply.github.com>
2025-03-07 20:29:39 +0100

    Add InputBlocker class for keyboard and mouse blocking

---
+++ Edulink.Client/Classes/InputBlocker.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Edulink.Classes
+{
+    public class InputBlocker : IDisposable
+    {
+        private static LowLevelKeyboardProc _keyboardProc = KeyboardHookCallback;
+        private static LowLevelMouseProc _mouseProc = MouseHookCallback;
+        private static IntPtr _keyboardHookID = IntPtr.Zero;
+        private static IntPtr _mouseHookID = IntPtr.Zero;
+
+        private static bool _blockKeyboard = false;
+        private static bool _blockMouse = false;
+
+        // Enable or disable keyboard blocking
+        public void BlockKeyboard(bool block)
+        {
+            _blockKeyboard = block;
+            if (block && _keyboardHookID == IntPtr.Zero)
+            {
+                _keyboardHookID = SetKeyboardHook(_keyboardProc);
+            }
+            else if (!block && _keyboardHookID != IntPtr.Zero)
+            {
+                UnhookWindowsHookEx(_keyboardHookID);
+                _keyboardHookID = IntPtr.Zero;
+            }
+        }
+
+        // Enable or disable mouse blocking
+        public void BlockMouse(bool block)
+        {
+            _blockMouse = block;
+            if (block && _mouseHookID == IntPtr.Zero)
+            {
+                _mouseHookID = SetMouseHook(_mouseProc);
+            }
+            else if (!block && _mouseHookID != IntPtr.Zero)
+            {
+                UnhookWindowsHookEx(_mouseHookID);
+                _mouseHookID = IntPtr.Zero;
+            }
+        }
+
+        private static IntPtr SetKeyboardHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private static IntPtr SetMouseHook(LowLevelMouseProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_MOUSE_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private static IntPtr KeyboardHookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && _blockKeyboard)
+            {
+                // Block all keyboard input
+                return (IntPtr)1;
+            }
+            return CallNextHookEx(_keyboardHookID, nCode, wParam, lParam);
+        }
+
+        private static IntPtr MouseHookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && _blockMouse)
+            {
+                // Block all mouse input
+                return (IntPtr)1;
+            }
+            return CallNextHookEx(_mouseHookID, nCode, wParam, lParam);
+        }
+
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WH_MOUSE_LL = 14;
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+        private delegate IntPtr LowLevelMouseProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelMouseProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        // Clean up hooks when the object is disposed
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (_keyboardHookID != IntPtr.Zero)
+                {
+                    UnhookWindowsHookEx(_keyboardHookID);
+                    _keyboardHookID = IntPtr.Zero;
+                }
+                if (_mouseHookID != IntPtr.Zero)
+                {
+                    UnhookWindowsHookEx(_mouseHookID);
+                    _mouseHookID = IntPtr.Zero;
+                }
+            }
+        }
+    }
+}
