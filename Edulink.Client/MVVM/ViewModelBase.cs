dcbe2b78cd214eb4e19e8d578f664daabe9c2508
Andrew <106198626+lxvdev@users.noreply.github.com>
2025-03-08 00:06:26 +0100

    Enhance ViewModelBase with error handling support
    Implemented INotifyDataErrorInfo in ViewModelBase to manage property validation errors. Added methods for adding, clearing, and retrieving errors, along with a HasErrors property and ErrorsChanged event for notifications. Updated using directives to include necessary namespaces.

--- Edulink.Client/MVVM/ViewModelBase.cs
+++ Edulink.Client/MVVM/ViewModelBase.cs
@@ -1,15 +1,62 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
 
 namespace Edulink.MVVM
 {
-    public class ViewModelBase : INotifyPropertyChanged
+    public class ViewModelBase : INotifyPropertyChanged, INotifyDataErrorInfo
     {
+        #region Data Error Info
+        private readonly Dictionary<string, List<string>> _propertyErrors = new Dictionary<string, List<string>>();
+
+        public bool HasErrors => _propertyErrors.Any();
+
+        public IEnumerable GetErrors(string propertyName)
+        {
+            if (_propertyErrors.TryGetValue(propertyName, out List<string> errors))
+            {
+                return errors;
+            }
+            return null;
+        }
+
+        public void AddError(string errorMessage, [CallerMemberName] string propertyName = null)
+        {
+            if (!_propertyErrors.ContainsKey(propertyName))
+            {
+                _propertyErrors[propertyName] = new List<string>();
+            }
+
+            _propertyErrors[propertyName].Add(errorMessage);
+            OnErrorsChanged(propertyName);
+        }
+
+        public void ClearErrors([CallerMemberName] string propertyName = null)
+        {
+            if (_propertyErrors.Remove(propertyName))
+            {
+                OnErrorsChanged(propertyName);
+            }
+        }
+
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+        private void OnErrorsChanged(string propertyName)
+        {
+            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+            OnPropertyChanged(nameof(HasErrors));
+        }
+        #endregion
+
+        #region Property Changed
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+        #endregion
     }
 }
