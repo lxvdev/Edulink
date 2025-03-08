1b02ed7ec6e68d8373e66a4e0cdd9c08b6b0d279
Andrew <106198626+lxvdev@users.noreply.github.com>
2025-03-08 18:10:20 +0100

    Added UpdaterDialog for updates

---
+++ Edulink.Server/Views/UpdaterDialog.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Edulink.Models;
+using Edulink.ViewModels;
+using System.Windows;
+
+namespace Edulink.Views
+{
+    /// <summary>
+    /// Interaction logic for UpdateDialog.xaml
+    /// </summary>
+    public partial class UpdaterDialog : Window
+    {
+        public UpdaterDialog(ReleaseDetails updateDetails = null, bool startUpdate = false)
+        {
+            InitializeComponent();
+
+            UpdaterDialogViewModel viewModel;
+            if (updateDetails == null)
+            {
+                viewModel = new UpdaterDialogViewModel();
+            }
+            else
+            {
+                viewModel = new UpdaterDialogViewModel(updateDetails);
+            }
+            viewModel.RequestClose += (sender, e) => Close();
+            DataContext = viewModel;
+            if (startUpdate)
+            {
+                if (viewModel.UpdateCommand.CanExecute(null))
+                {
+                    viewModel.UpdateCommand.Execute(null);
+                }
+                else { Close(); }
+            }
+        }
+    }
+}
