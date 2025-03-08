4889c928665ff25ef6dc8aa03204b7fadf9beafe
Andrew <106198626+lxvdev@users.noreply.github.com>
2025-03-08 11:53:27 +0100

    Add localization support with new config and strings classes
    Introduces `LocalizationConfig` and `LocalizedStrings` classes in the `Edulink.Classes` namespace. `LocalizationConfig` manages localization settings such as `DesignCulture`, `DefaultAssembly`, and `DefaultDictionary`. `LocalizedStrings` implements a singleton pattern to retrieve localized strings using the `WPFLocalizeExtension` library.

---
+++ Edulink.Server/Classes/LocalizationConfig.cs
@@ -0,0 +1,12 @@
+﻿using Edulink.Languages;
+using System.Reflection;
+
+namespace Edulink.Classes
+{
+    public static class LocalizationConfig
+    {
+        public static string DesignCulture { get; set; } = "en-US";
+        public static string DefaultAssembly { get; set; } = Assembly.GetExecutingAssembly().FullName;
+        public static string DefaultDictionary { get; set; } = nameof(Strings);
+    }
+}
---
+++ Edulink.Server/Classes/LocalizedStrings.cs
@@ -0,0 +1,19 @@
+﻿using Edulink.Languages;
+using System.Reflection;
+using WPFLocalizeExtension.Engine;
+
+namespace Edulink.Classes
+{
+    public class LocalizedStrings
+    {
+        public static LocalizedStrings Instance { get; } = new LocalizedStrings();
+
+        public string this[string key]
+        {
+            get
+            {
+                return LocalizeDictionary.Instance.GetLocalizedObject(Assembly.GetExecutingAssembly().GetName().Name, nameof(Strings), key, LocalizeDictionary.Instance.Culture).ToString();
+            }
+        }
+    }
+}
