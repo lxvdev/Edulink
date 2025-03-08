bc70aec75d6f53c753c5c9a8e56795c28b8eb656
Andrew <106198626+lxvdev@users.noreply.github.com>
2025-03-08 11:54:57 +0100

    Add CheckForUpdates setting in Settings.cs

--- Edulink.Server/Models/Settings.cs
+++ Edulink.Server/Models/Settings.cs
@@ -5,6 +5,7 @@
         public int Port { get; set; } = 7153;
         public bool PreviewEnabled { get; set; } = true;
         public double PreviewFrequency { get; set; } = 5000;
+        public bool CheckForUpdates { get; set; } = true;
         public string Language { get; set; } = null;
         public string Theme { get; set; } = "Auto";
     }
