edc2e2b5198dbfeda5feaed009e64758b8977a1b
Andrew <106198626+lxvdev@users.noreply.github.com>
2025-03-12 16:26:19 +0100

    Add TrackableValidatableClosableViewModel.cs
    - Moved logic from SettingsViewModel to this base for cleaner code

---
+++ Edulink.Server/MVVM/TrackableValidatableClosableViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace Edulink.MVVM
+{
+    public class TrackableValidatableClosableViewModel : ValidatableClosableViewModel
+    {
+        private readonly HashSet<string> _unsavedProperties = new HashSet<string>();
+
+        public bool HasUnsavedChanges => _unsavedProperties.Count > 0;
+
+        protected void TrackUnsavedChanges(object originalValue, [CallerMemberName] string propertyName = null)
+        {
+            object newValue = GetType().GetProperty(propertyName)?.GetValue(this);
+
+            if (!Equals(originalValue, newValue))
+            {
+                _unsavedProperties.Add(propertyName);
+            }
+            else
+            {
+                _unsavedProperties.Remove(propertyName);
+            }
+
+            OnPropertyChanged(nameof(HasUnsavedChanges));
+        }
+
+        protected void ClearUnsavedChanges()
+        {
+            _unsavedProperties.Clear();
+            OnPropertyChanged(nameof(HasUnsavedChanges));
+        }
+    }
+}
