a067b00daf7add5ce5df93a61d4d8b6c3731f5fe
Andrew <106198626+lxvdev@users.noreply.github.com>
2025-03-12 15:56:55 +0100

    Add ValidatableViewModel.cs
    - Moved Validation logic from ViewModelBase to here

---
+++ Edulink.Client/MVVM/ValidatableViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace Edulink.MVVM
+{
+    public class ValidatableViewModel : ViewModelBase, INotifyDataErrorInfo
+    {
+        private readonly Dictionary<string, List<string>> _propertyErrors = new Dictionary<string, List<string>>();
+
+        public bool HasErrors => _propertyErrors.Any();
+
+        public IEnumerable GetErrors(string propertyName)
+        {
+            if (_propertyErrors.TryGetValue(propertyName, out List<string> errors))
+            {
+                return errors;
+            }
+            return null;
+        }
+
+        public void AddError(string errorMessage, [CallerMemberName] string propertyName = null)
+        {
+            if (!_propertyErrors.ContainsKey(propertyName))
+            {
+                _propertyErrors[propertyName] = new List<string>();
+            }
+
+            _propertyErrors[propertyName].Add(errorMessage);
+            OnErrorsChanged(propertyName);
+        }
+
+        public void ClearErrors([CallerMemberName] string propertyName = null)
+        {
+            if (_propertyErrors.Remove(propertyName))
+            {
+                OnErrorsChanged(propertyName);
+            }
+        }
+
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+        private void OnErrorsChanged(string propertyName)
+        {
+            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+            OnPropertyChanged(nameof(HasErrors));
+        }
+    }
+}
