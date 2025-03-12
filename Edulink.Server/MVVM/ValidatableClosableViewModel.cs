a404dcd0bd7631ce727c7acf8ad650b31a7d29da
Andrew <106198626+lxvdev@users.noreply.github.com>
2025-03-12 17:12:38 +0100

    Update Closable ViewModels
    - Reverted protection level from protected to public on event

--- Edulink.Server/MVVM/ValidatableClosableViewModel.cs
+++ Edulink.Server/MVVM/ValidatableClosableViewModel.cs
@@ -4,13 +4,13 @@ namespace Edulink.MVVM
 {
     public class ValidatableClosableViewModel : ValidatableViewModel
     {
-        protected event EventHandler RequestClose;
+        public event EventHandler RequestClose;
         protected virtual void OnRequestClose()
         {
             RequestClose?.Invoke(this, EventArgs.Empty);
         }
 
-        protected event EventHandler<bool> RequestDialogClose;
+        public event EventHandler<bool> RequestDialogClose;
         protected virtual void OnRequestDialogClose(bool dialogResult)
         {
             RequestDialogClose?.Invoke(this, dialogResult);
