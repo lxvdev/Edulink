61402241c5a6e9aa21048fac711c54c4b8c00224
Andrew <106198626+lxvdev@users.noreply.github.com>
2025-03-12 15:59:38 +0100

    Add ValidatableClosableViewModel.cs
    - Inherits Validation logic and has requesting window close logic

---
+++ Edulink.Client/MVVM/ValidatableClosableViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Edulink.MVVM
+{
+    public class ValidatableClosableViewModel : ValidatableViewModel
+    {
+        public event EventHandler RequestClose;
+        protected virtual void OnRequestClose()
+        {
+            RequestClose?.Invoke(this, EventArgs.Empty);
+        }
+
+        public event EventHandler<bool> RequestDialogClose;
+        protected virtual void OnRequestDialogClose(bool dialogResult)
+        {
+            RequestDialogClose?.Invoke(this, dialogResult);
+        }
+    }
+}
