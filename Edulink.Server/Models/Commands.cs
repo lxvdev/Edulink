d8d5e2c917bbf7bd4d14ee36f016b3238ac412a5
Andrew <106198626+lxvdev@users.noreply.github.com>
2025-03-12 21:05:10 +0100

    Update Commands.cs
    - Added a new toggle mute command
    - Added version and name to commands

--- Edulink.Server/Models/Commands.cs
+++ Edulink.Server/Models/Commands.cs
@@ -1,22 +1,48 @@
-﻿namespace Edulink.Models
+﻿using System;
+
+namespace Edulink.Models
 {
     public class Commands
     {
-        public const string Link = "LINK";
-        public const string Message = "MESSAGE";
-        public const string Desktop = "DESKTOP";
-        public const string RestartApplication = "RESTARTAPP";
-        public const string Shutdown = "SHUTDOWN";
-        public const string Restart = "RESTART";
-        public const string LockScreen = "LOCKSCREEN";
-        public const string LogOff = "LOGOFF";
-        public const string RenameComputer = "RENAME";
-        public const string Update = "UPDATE";
-        public const string ResetPassword = "RESETPASSWORD";
-        public const string BlockInput = "BLOCKINPUT";
-        public const string Preview = "PREVIEW";
+        public static readonly Command Link = new Command("LINK", "2.0.0.0");
+        public static readonly Command Message = new Command("MESSAGE", "2.0.0.0");
+        public static readonly Command ViewDesktop = new Command("DESKTOP", "2.0.0.0");
+        public static readonly Command RestartApplication = new Command("RESTARTAPP", "2.0.0.0");
+        public static readonly Command Shutdown = new Command("SHUTDOWN", "2.0.0.0");
+        public static readonly Command Restart = new Command("RESTART", "2.0.0.0");
+        public static readonly Command LockScreen = new Command("LOCKSCREEN", "2.0.0.0");
+        public static readonly Command LogOff = new Command("LOGOFF", "2.0.0.0");
+        public static readonly Command RenameComputer = new Command("RENAME", "2.0.0.0");
+        public static readonly Command Update = new Command("UPDATE", "2.0.0.0");
+        public static readonly Command ResetPassword = new Command("RESETPASSWORD", "2.0.0.0");
+        public static readonly Command BlockInput = new Command("BLOCKINPUT", "2.0.0.0");
+        public static readonly Command Preview = new Command("PREVIEW", "2.0.0.0");
 
-        public const string Connect = "CONNECT";
-        public const string Disconnect = "DISCONNECT";
+        public static readonly Command ToggleMute = new Command("TOGGLEMUTE", "2.0.1.0");
+
+        public static readonly Command Connect = new Command("CONNECT", "2.0.0.0");
+        public static readonly Command Disconnect = new Command("DISCONNECT", "2.0.0.0");
+    }
+
+    public class Command
+    {
+        public string Name { get; }
+        public Version MinimumVersion { get; }
+
+        public Command(string name)
+        {
+            Name = name;
+        }
+
+        public Command(string name, string minimumVersion)
+        {
+            Name = name;
+            MinimumVersion = new Version(minimumVersion);
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
     }
 }
