731420e42ee784b931139840d865815d4e489eed
Andrew <106198626+lxvdev@users.noreply.github.com>
2025-03-12 19:34:58 +0100

    Add AudioControl.cs
    - Added a new class for controlling muting audio

---
+++ Edulink.Client/Classes/AudioControl.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Edulink.Classes
+{
+    public class AudioControl
+    {
+        private const uint WM_APPCOMMAND = 0x0319;
+        private const uint APPCOMMAND_MUTE = 0x80000;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern int SendMessage(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern IntPtr GetForegroundWindow();
+
+        public static void ToggleMuteAudio()
+        {
+            IntPtr hwnd = GetForegroundWindow();
+            SendMessage(hwnd, WM_APPCOMMAND, hwnd, (IntPtr)APPCOMMAND_MUTE);
+        }
+    }
+}
