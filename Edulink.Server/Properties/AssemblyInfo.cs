c286c4d747d16e09f8df6e19482e2809fe76309f
Andrew <106198626+lxvdev@users.noreply.github.com>
2025-03-13 18:43:40 +0100

    Update AssemblyInfo.cs
    - Changed to the right copyright
    - Updated version

--- Edulink.Server/Properties/AssemblyInfo.cs
+++ Edulink.Server/Properties/AssemblyInfo.cs
@@ -1,6 +1,4 @@
 ﻿using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 
@@ -12,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Edulink Server")]
-[assembly: AssemblyCopyright("Copyright © lxvdev 2024-2025")]
+[assembly: AssemblyCopyright("Copyright ©Andrew 2024-2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.0.1.0")]
+[assembly: AssemblyFileVersion("2.0.1.0")]
