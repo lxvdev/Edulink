1e4dddd750d198ab9f88f03b0401b3370a66fb33
Andrew <106198626+lxvdev@users.noreply.github.com>
2025-03-15 23:18:15 +0100

    Add SendFileWindow.xaml.cs
    - Manages updating list and sharing status of the view model

---
+++ Edulink.Client/Views/SendFileWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Edulink.Models;
+using Edulink.ViewModels;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace Edulink.Views
+{
+    /// <summary>
+    /// Interaction logic for SendFileWindow.xaml
+    /// </summary>
+    public partial class SendFileWindow : Window
+    {
+        private SendFileWindowViewModel _viewModel;
+
+        public SendFileWindow()
+        {
+            InitializeComponent();
+            _viewModel = new SendFileWindowViewModel();
+            DataContext = _viewModel;
+        }
+
+        public void UpdateList(List<Computer> computerList)
+        {
+            _viewModel.UpdateList(computerList);
+        }
+
+        public void SetSharingStatus(bool enabled)
+        {
+            _viewModel.SetSharingStatus(enabled);
+        }
+    }
+}
