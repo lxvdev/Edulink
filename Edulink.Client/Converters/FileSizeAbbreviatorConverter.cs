53225debc42600e20610aeb2309e400b18e02c60
Andrew <106198626+lxvdev@users.noreply.github.com>
2025-03-24 20:43:02 +0100

    Add FileSizeAbbreviatorConverter.cs
    - Added for abbreviating large file size numbers

---
+++ Edulink.Client/Converters/FileSizeAbbreviatorConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Edulink.Converters
+{
+    public class FileSizeAbbreviatorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is long bytes)
+            {
+                string[] sizes = { "B", "KB", "MB", "GB", "TB" };
+                double len = bytes;
+                int order = 0;
+
+                while (len >= 1024 && order < sizes.Length - 1)
+                {
+                    order++;
+                    len /= 1024;
+                }
+
+                return $"{len:0.##} {sizes[order]}";
+            }
+            return "0 B";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
