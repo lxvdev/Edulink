985b11b84e38af343306edad4e0ba3df41eeb028
Andrew <106198626+lxvdev@users.noreply.github.com>
2025-03-24 20:49:36 +0100

    Add SelectComputerControl
    - Added for handling computer selection

---
+++ Edulink.Client/Views/SelectComputerControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Edulink.Views
+{
+    /// <summary>
+    /// Interaction logic for SelectComputerControl.xaml
+    /// </summary>
+    public partial class SelectComputerControl : UserControl
+    {
+        public SelectComputerControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
