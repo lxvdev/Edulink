172e74f195b38c313d65e0209fc625ac0073fe63
Andrew <106198626+lxvdev@users.noreply.github.com>
2025-03-24 20:47:07 +0100

    Add SharingFile.cs model
    - Added for information about the file that is going to be shared or its being shared

---
+++ Edulink.Client/Models/SharingFile.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+namespace Edulink.Models
+{
+    public class SharingFile
+    {
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+        public long FileSize { get; set; }
+
+        public SharingFile(string filePath)
+        {
+            FilePath = filePath;
+            FileName = Path.GetFileName(filePath);
+            FileSize = new FileInfo(filePath).Length;
+        }
+    }
+}
