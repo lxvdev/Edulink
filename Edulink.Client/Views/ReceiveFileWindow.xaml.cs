402ac5e42479ede3c9afa5f96b74c5976af2210e
Andrew <106198626+lxvdev@users.noreply.github.com>
2025-03-25 22:27:46 +0100

    Add ReceiveFileWindow.xaml.cs
    This commit introduces the `ReceiveFileWindow` class in the `Edulink.Views` namespace, implementing the interaction logic for the associated XAML file. It includes necessary using directives and initializes a view model (`ReceiveFileWindowViewModel`) in the constructor. The class also sets up event handling for a request accept event, with a placeholder method `_viewModel_RequestAccept` to manage the accept action.

---
+++ Edulink.Client/Views/ReceiveFileWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Edulink.Models;
+using Edulink.ViewModels;
+using System;
+using System.IO;
+using System.Windows;
+
+namespace Edulink.Views
+{
+    /// <summary>
+    /// Interaction logic for ReceiveFileWindow.xaml
+    /// </summary>
+    public partial class ReceiveFileWindow : Window
+    {
+        private ReceiveFileWindowViewModel _viewModel;
+
+        public ReceiveFileWindow(Computer sourceComputer, FileInfo fileInfo)
+        {
+            InitializeComponent();
+            _viewModel = new ReceiveFileWindowViewModel(sourceComputer, fileInfo);
+            _viewModel.RequestAccept += _viewModel_RequestAccept;
+            DataContext = _viewModel;
+        }
+
+        private void _viewModel_RequestAccept(object sender, EventArgs e)
+        {
+            // TODO: Handle accept
+        }
+    }
+}
