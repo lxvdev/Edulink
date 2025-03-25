c288565f1de489d40fa786afb213eceafe3cb7b1
Andrew <106198626+lxvdev@users.noreply.github.com>
2025-03-25 22:24:17 +0100

    Add ReceiveFileWindowViewModel
    This class manages file reception state with properties like ReceivingFile, SourceComputer, and File.
    It includes an event RequestAccept for file acceptance and a command AcceptCommand to handle the acceptance process.

---
+++ Edulink.Client/ViewModels/ReceiveFileWindowViewModel.cs
@@ -0,0 +1,73 @@
+﻿using Edulink.Classes;
+using Edulink.Models;
+using Edulink.MVVM;
+using System;
+using System.IO;
+using System.Windows.Input;
+
+namespace Edulink.ViewModels
+{
+    public class ReceiveFileWindowViewModel : ViewModelBase
+    {
+        private bool _receivingFile;
+        public bool ReceivingFile
+        {
+            get => _receivingFile;
+            set
+            {
+                if (_receivingFile != value)
+                {
+                    _receivingFile = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private Computer _sourceComputer;
+        public Computer SourceComputer
+        {
+            get => _sourceComputer;
+            set
+            {
+                if (_sourceComputer != value)
+                {
+                    _sourceComputer = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string SourceName => SourceComputer?.IsTeacher == false ? SourceComputer.Name : LocalizedStrings.Instance["ReceiveFile.ReceiveFileFrom.Teacher"];
+
+        private FileInfo _file;
+        public FileInfo File
+        {
+            get => _file;
+            set
+            {
+                if (_file != value)
+                {
+                    _file = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event EventHandler RequestAccept;
+
+        public ReceiveFileWindowViewModel(Computer sourceComputer, FileInfo fileInfo)
+        {
+            _sourceComputer = sourceComputer;
+            _file = fileInfo;
+        }
+
+        public ReceiveFileWindowViewModel() { }
+
+        public ICommand AcceptCommand => new RelayCommand(execute => HandleAccept());
+
+        private void HandleAccept()
+        {
+            RequestAccept?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
