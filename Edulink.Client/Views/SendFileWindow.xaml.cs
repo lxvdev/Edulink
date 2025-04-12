5a9dc9f02bab63f61d01bf2dc0ec75ac5192b5cb
Andrew <106198626+lxvdev@users.noreply.github.com>
2025-04-13 00:12:49 +0200

    Enhance file sharing functionality in SendFileWindow
    Updated `SendFileWindow.xaml.cs` to improve file sharing capabilities. Added a `FileSharing` instance for managing file transfers, enhanced connection status handling, and implemented error handling for file selection and transfer processes. The `RequestSendFile` method now includes timeout management and response handling. Additionally, file selection logic has been improved with checks for file existence and size limitations to enhance user experience.

--- Edulink.Client/Views/SendFileWindow.xaml.cs
+++ Edulink.Client/Views/SendFileWindow.xaml.cs
@@ -1,12 +1,16 @@
-﻿using Edulink.Communication.Models;
+﻿using Edulink.Communication;
+using Edulink.Communication.Models;
 using Edulink.Models;
 using Edulink.ViewModels;
 using Microsoft.Win32;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -19,42 +23,58 @@ namespace Edulink.Views
     {
         private SendFileWindowViewModel _viewModel;
 
+        private FileSharing _fileSharing;
+
         public SendFileWindow()
         {
             InitializeComponent();
             _viewModel = new SendFileWindowViewModel();
+            _fileSharing = new FileSharing();
             DataContext = _viewModel;
 
-            App.Client.ConnectionStatusChanged += async (sender, e) => await Client_ConnectionStatusChangedAsync(sender, e);
+            // Request computer list whenever the connection status changes
+            App.Client.ConnectionStatusChanged += (_, status) =>
+            {
+                _viewModel.PageIndex = 0;
+            };
 
-            _ = RequestComputerList();
+            // Request sending file to the target computer
+            _viewModel.RequestSendFile += async (_, e) => await RequestSendFile();
+
+            _viewModel.RequestComputerList += async (_, e) => await RequestComputerList();
+
+            // File sharing progress
+            _fileSharing.ProgressChanged += (_, bytes) => _viewModel.Progress = (int)((double)bytes / _viewModel.File.Length * 100);
+
+            _fileSharing.ConnectionChanged += (_, connected) =>
+            {
+                if (connected)
+                {
+                    _viewModel.IsSendingFile = true;
+                }
+            };
         }
 
-        private async Task Client_ConnectionStatusChangedAsync(object sender, bool e)
-        {
-            if (!App.Client.Connected) _viewModel.SetReceivingStatus(false);
 
-            await RequestComputerList();
-        }
-
+        #region Receiving list
         private async Task RequestComputerList()
         {
             try
             {
                 if (App.Client.Connected)
                 {
-                    _viewModel.SetReceivingStatus(true);
+                    _viewModel.IsReceivingList = true;
                     await App.Client.Helper?.SendCommandAsync(new EdulinkCommand() { Command = Commands.ComputerList.ToString() });
                 }
                 else
                 {
-                    _viewModel.SetReceivingStatus(false);
+                    _viewModel.IsReceivingList = false;
                 }
             }
             catch (Exception ex)
             {
                 Debug.WriteLine($"Could not request computer list: {ex.Message}");
-                _viewModel.SetReceivingStatus(false);
+                _viewModel.IsReceivingList = false;
             }
         }
 
@@ -67,8 +87,9 @@ namespace Edulink.Views
         {
             _viewModel.SetSharingStatus(student, teacher);
         }
+        #endregion
 
-        // TODO: Create a "File" model
+        #region Selecting file
         private void SelectFileButton_Drop(object sender, DragEventArgs e)
         {
             if (e.Data.GetDataPresent(DataFormats.FileDrop))
@@ -91,11 +112,178 @@ namespace Edulink.Views
             SetFile(openFileDialog.FileName);
         }
 
-        private void SetFile(string filePath)
+        private void SetFile(string path)
         {
-            if (filePath == null) return;
+            if (path == null) throw new ArgumentNullException(nameof(path));
 
-            _viewModel.SetFile(new FileInfo(filePath));
+            try
+            {
+                FileInfo fileInfo = new FileInfo(path);
+
+                if (!fileInfo.Exists)
+                {
+                    Debug.WriteLine("Invalid file path provided.");
+                    MessageDialog.ShowLocalized("Message.Content.InvalidFilePath", MessageDialogTitle.Error, MessageDialogButton.Ok, MessageDialogIcon.Error);
+                    return;
+                }
+
+                if (fileInfo.Length > 200 * 1024 * 1024) // 200MB
+                {
+                    MessageDialog.ShowLocalized("Message.Content.TheFileCannotBeLargerThan200MB", MessageDialogTitle.Warning, MessageDialogButton.Ok, MessageDialogIcon.Warning);
+                    return;
+                }
+
+                // Check if the file is accessible
+                using (FileStream stream = fileInfo.Open(FileMode.Open, FileAccess.Read, FileShare.None))
+                {
+                    _viewModel.SetFile(fileInfo);
+                }
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                Debug.WriteLine($"Unauthorized access: {ex.Message}");
+                MessageDialog.ShowLocalized("Message.Content.UnauthorizedAccess", MessageDialogTitle.Error, MessageDialogButton.Ok, MessageDialogIcon.Error);
+            }
+            catch (PathTooLongException ex)
+            {
+                Debug.WriteLine($"Path too long: {ex.Message}");
+                MessageDialog.ShowLocalized("Message.Content.PathTooLong", MessageDialogTitle.Error, MessageDialogButton.Ok, MessageDialogIcon.Error);
+            }
+            catch (IOException ex)
+            {
+                Debug.WriteLine($"File I/O error: {ex.Message}");
+                MessageDialog.ShowLocalized("Message.Content.FileInUse", MessageDialogTitle.Error, MessageDialogButton.Ok, MessageDialogIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Unexpected error: {ex.Message}");
+                MessageDialog.ShowLocalized("Message.Content.UnexpectedError", MessageDialogTitle.Error, MessageDialogButton.Ok, MessageDialogIcon.Error);
+            }
         }
+        #endregion
+
+        #region Sending file
+        private CancellationTokenSource _responseTimeoutCts;
+
+        public async Task RequestSendFile()
+        {
+            try
+            {
+                if (App.Client.Connected)
+                {
+                    _viewModel.IsReceivingResponse = true;
+                    await App.Client.Helper?.SendCommandAsync(new EdulinkCommand()
+                    {
+                        Command = Commands.RequestSendFile.ToString(),
+                        Parameters = new Dictionary<string, string>()
+                        {
+                            { "TargetComputer", JsonConvert.SerializeObject(_viewModel.TargetComputer) },
+                            { "FileName", _viewModel.File.Name },
+                            { "FileLength", _viewModel.File.Length.ToString() }
+                        }
+                    });
+
+                    // Start response timeout
+                    _responseTimeoutCts = new CancellationTokenSource();
+
+                    await Task.Delay(30000, _responseTimeoutCts.Token);
+
+                    _viewModel.IsReceivingResponse = false;
+
+                    _viewModel.Reset();
+                }
+                else
+                {
+                    _viewModel.IsReceivingResponse = false;
+                }
+            }
+            catch (TaskCanceledException)
+            {
+                Debug.WriteLine("Response received");
+                _viewModel.IsReceivingResponse = false;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Could not request send file: {ex.Message}");
+                _viewModel.IsReceivingResponse = false;
+            }
+        }
+
+        public async void HandleResponse(bool accepted, IEnumerable<IPEndPoint> ipEndPoints = null)
+        {
+            _responseTimeoutCts?.Cancel();
+
+            try
+            {
+                if (accepted && ipEndPoints != null)
+                {
+                    bool success = false;
+                    foreach (IPEndPoint ipEndPoint in ipEndPoints)
+                    {
+                        success = await SendFile(ipEndPoint.Address, ipEndPoint.Port);
+                        if (success)
+                        {
+                            break;
+                        }
+                    }
+
+                    if (!success)
+                    {
+                        throw new InvalidOperationException("Could not send file to any of the provided endpoints.");
+                    }
+                }
+                else
+                {
+                    MessageDialog.ShowLocalized("Message.Content.FileTransferDeclined", MessageDialogTitle.Warning, MessageDialogButton.Ok, MessageDialogIcon.Warning);
+                }
+            }
+            catch (IOException ex)
+            {
+                Debug.WriteLine($"File I/O error: {ex.Message}");
+                MessageDialog.ShowLocalized("Message.Content.CouldNotSendFile", MessageDialogTitle.Error, MessageDialogButton.Ok, MessageDialogIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Unexpected error: {ex.Message}");
+                MessageDialog.ShowLocalized("Message.Content.CouldNotSendFile", MessageDialogTitle.Error, MessageDialogButton.Ok, MessageDialogIcon.Error);
+            }
+        }
+
+        public async Task<bool> SendFile(IPAddress ipAddress, int port)
+        {
+            if (_viewModel?.TargetComputer == null || _viewModel?.File == null)
+            {
+                throw new InvalidOperationException("Invalid target computer or file.");
+            }
+
+            try
+            {
+                bool success = await _fileSharing.SendFileAsync(_viewModel.File, ipAddress, port);
+                if (success)
+                {
+                    _viewModel.IsFileSent = true;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (IOException ex)
+            {
+                Debug.WriteLine($"File I/O error: {ex.Message}");
+                throw;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Unexpected error: {ex.Message}");
+                throw;
+            }
+            finally
+            {
+                _viewModel.IsSendingFile = false;
+            }
+        }
+        #endregion
     }
 }
