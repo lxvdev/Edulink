5cd82208187d97ccfd537f258c0b5192b5cf2eb6
Andrew <106198626+lxvdev@users.noreply.github.com>
2025-04-13 00:12:01 +0200

    Enhance file sharing functionality in ReceiveFileWindow
    Updated ReceiveFileWindow to support asynchronous file sharing.
    Modified constructor to accept file name and length, added
    FileSharing instance, and changed event handler to async.
    Implemented server start, directory check, command sending,
    and file receiving asynchronously.

--- Edulink.Client/Views/ReceiveFileWindow.xaml.cs
+++ Edulink.Client/Views/ReceiveFileWindow.xaml.cs
@@ -1,7 +1,16 @@
-﻿using Edulink.Models;
+﻿using Edulink.Classes;
+using Edulink.Communication;
+using Edulink.Communication.Models;
+using Edulink.Converters;
+using Edulink.Models;
 using Edulink.ViewModels;
+using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace Edulink.Views
@@ -13,17 +22,50 @@ namespace Edulink.Views
     {
         private ReceiveFileWindowViewModel _viewModel;
 
-        public ReceiveFileWindow(Computer sourceComputer, FileInfo fileInfo)
+        private FileSharing _fileSharing;
+
+        private readonly int _port = App.SettingsManager.Settings.FileSharingPort;
+
+        public ReceiveFileWindow(Computer sourceComputer, string fileName, long fileLength)
         {
             InitializeComponent();
-            _viewModel = new ReceiveFileWindowViewModel(sourceComputer, fileInfo);
-            _viewModel.RequestAccept += _viewModel_RequestAccept;
+            _viewModel = new ReceiveFileWindowViewModel(sourceComputer, fileName, fileLength, Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\Downloads\Edulink Transfers");
+            _viewModel.RequestAccept += ViewModel_RequestAcceptAsync;
+            _fileSharing = new FileSharing();
             DataContext = _viewModel;
         }
 
-        private void _viewModel_RequestAccept(object sender, EventArgs e)
+        private async void ViewModel_RequestAcceptAsync(object sender, EventArgs e)
         {
             // TODO: Handle accept
+            if (App.Client.Connected)
+            {
+                IEnumerable<IPEndPoint> ipEndPoints = IPAddressProvider.GetIPAddresses().Select(ip => new IPEndPoint(ip, _port));
+
+                // Start the server asynchronously
+                await Task.Run(() => _fileSharing.StartServer(_port));
+
+                // Ensure the save directory exists
+                if (!Directory.Exists(_viewModel.SaveDirectory))
+                {
+                    Directory.CreateDirectory(_viewModel.SaveDirectory);
+                }
+
+                // Send the command asynchronously
+                await App.Client.Helper.SendCommandAsync(new EdulinkCommand()
+                {
+                    Command = Commands.ResponseSendFile.ToString(),
+                    Parameters = new Dictionary<string, string>()
+                    {
+                        { "Accepted", true.ToString() },
+                        { "TargetComputer", JsonConvert.SerializeObject(_viewModel.SourceComputer) },
+                        { "IP", JsonConvert.SerializeObject(ipEndPoints, new IPEndPointConverter()) }
+                    }
+                });
+
+                // Receive the file asynchronously
+                await _fileSharing.ReceiveFileAsync(_viewModel.SaveDirectory, _viewModel.FileName);
+            }
         }
     }
 }
