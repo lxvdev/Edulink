0b85164dd204b108388afa3fd772eac27bc5098e
Andrew <106198626+lxvdev@users.noreply.github.com>
2025-04-13 00:10:18 +0200

    Add FileSharingPort property to Settings class
    Introduced a new `FileSharingPort` property of type `int` in the `Settings` class, with a default value set to `8073`.

--- Edulink.Client/Models/Settings.cs
+++ Edulink.Client/Models/Settings.cs
@@ -5,6 +5,7 @@
         public string Name { get; set; } = null;
         public string IPAddress { get; set; } = null;
         public int Port { get; set; } = 7153;
+        public int FileSharingPort { get; set; } = 8073;
         public bool CheckForUpdates { get; set; } = true;
         public string Language { get; set; } = null;
         public string Password { get; set; } = null;
