5448208618b97a33112958716b7138a4346f9ef3
Andrew <106198626+lxvdev@users.noreply.github.com>
2025-04-13 00:09:09 +0200

    Add IPEndPointConverter for JSON serialization
    Introduces a new `IPEndPointConverter` class in the
    `Edulink.Converters` namespace to handle custom
    serialization and deserialization of `IPEndPoint`
    objects. Implements `ReadJson` and `WriteJson` methods
    to convert between JSON format and `IPEndPoint` instances.

---
+++ Edulink.Server/Converters/IPEndPointConverter.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net;
+
+namespace Edulink.Converters
+{
+    public class IPEndPointConverter : JsonConverter<IPEndPoint>
+    {
+        public override IPEndPoint ReadJson(JsonReader reader, Type objectType, IPEndPoint existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            JObject jObject = JObject.Load(reader);
+
+            IPAddress address = IPAddress.Parse(jObject["Address"].Value<string>());
+            int port = jObject["Port"].Value<int>();
+
+            return new IPEndPoint(address, port);
+        }
+
+        public override void WriteJson(JsonWriter writer, IPEndPoint value, JsonSerializer serializer)
+        {
+            writer.WriteStartObject();
+            writer.WritePropertyName("Address");
+            writer.WriteValue(value.Address.ToString());
+            writer.WritePropertyName("Port");
+            writer.WriteValue(value.Port);
+            writer.WriteEndObject();
+        }
+    }
+}
